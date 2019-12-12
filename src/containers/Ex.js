614934f90c73e7659cfcef6908765869ff114906
JMOSQUER <mosquera@aliados.proteccion.com.co>
2019-12-12 18:05:18 -0500

    Anexo cambios del proyecto

--- src/containers/Ex.js
+++ src/containers/Ex.js
@@ -2,6 +2,9 @@ import React from 'react';
 import NavBar from '../components/Prueba';
 import Footer from '../components/Footer';
 import Main from '../components/Main';
+import rick from '../components/images/rick.jpg';
+
+
 
 const Ex = () => {
     return (
@@ -13,35 +16,28 @@ const Ex = () => {
             <section class="showcase_students">
                 <div className="container">
                     <div className="card mb-4 shadow-sm">
-                        <div className="card-body">
-                            <article className='characterEst'>
-                                <div>
-                                    <h1 className="card-title pricing-card-title">Foto</h1>
-                                    <p>Nataly</p>
+                        <div className="card-header-Students">
+                            <img src={rick} alt='estudiante' />
+                            <div classname='character_title'>
+                                <div className='character_name'>
+                                    <p>Rick</p>
                                 </div>
-                                <div className="charactercard-info">
-                                    <div>
-                                        <span>estado</span>
-                                        <p>Proceso</p>
-                                    </div>
-                                </div>
-                            </article>
-                            <article className='characterEst'>
-                                <div>
-                                    <h1 className="card-title pricing-card-title">Foto</h1>
-                                    <p>Nataly</p>
-                                </div>
-                                <div className="charactercard-info">
-                                    <div>
-                                        <p>estado</p>
-                                        <span>Proceso</span>
-                                    </div>
-                                </div>
-                            </article>
-            
+                            </div>
                         </div>
+                        <div className="card-body">
+                            <h1 className="card-title pricing-card-title">Usuario</h1>
+                            <input type="text" name="usuario" className="login-input" placeholder="Usuario" /><br />
 
-
+                            <h1 className="card-title pricing-card-title">Contraseña</h1>
+                            <input
+                                type="password"
+                                name="password"
+                                className="login-input"
+                                placeholder="Contraseña" /><br /><br />
+                            <a type="button" className="btn btn-lg btn-block btn-primary" id="Gest_Tar" href='http://localhost:3000/ModuloDoc'>Inicio</a>
+                            <br />
+                            <a href="#" >Olvide mi contraseña</a>
+                        </div>
                     </div>
                 </div>
             </section>
--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,43 +1,42 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
-class Login extends React.Component{
+class Login extends React.Component {
 
-    constructor(props){
+    constructor(props) {
         super(props)
     }
-   
 
-    
-    render(){
-        return(
+
+
+    render() {
+        return (
             <>
-            <div>
-            </div>
+                <div>
+                </div>
                 <div className="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
                     <h1 className="display-4">Login</h1>
-                   </div>
+                </div>
                 <div className="container">
-                        <div className="card mb-4 shadow-sm">
+                    <div className="card mb-4 shadow-sm">
                         <div className="card-header">
                             <h4 className="my-0 font-weight-normal">Inicie sesión</h4>
                         </div>
                         <div className="card-body">
                             <h1 className="card-title pricing-card-title">Usuario</h1>
-                            <input type="text" name="usuario" className="login-input" placeholder="Usuario"/><br/><br/>
-                
-                           <h1 className="card-title pricing-card-title">Contraseña</h1>
+                            <input type="text" name="usuario" className="login-input" placeholder="Usuario" /><br /><br />
+
+                            <h1 className="card-title pricing-card-title">Contraseña</h1>
                             <input
-                             type="password"
-                            name="password"
-                            className="login-input"
-                            placeholder="Contraseña"/><br/><br/>
+                                type="password"
+                                name="password"
+                                className="login-input"
+                                placeholder="Contraseña" /><br /><br />
                             <a type="button" className="btn btn-lg btn-block btn-primary" id="Gest_Tar" href='http://localhost:3000/ModuloDoc'>Inicio</a>
-                            <br/>
-                         <a href="#" >Olvide mi contraseña</a>
-                        </div>
-                        
+                            <br />
+                            <a href="#" >Olvide mi contraseña</a>
                         </div>
                     </div>
+                </div>
             </>
         )
     }
